c9fdd3bb6c83bab3dd2103a4df3374de24611a75
GMMarshall <gmmarshall27@gmail.com>
2021-09-22 15:42:50 -0400

    got first question to appear once start button is clicked

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
+var nextQuestionIndex=0;
 var startButton = document.getElementById('startBtn');
 startButton.addEventListener('click', startQuiz);
+var answersEl = document.getElementById('answers')
+var questionsEl = document.getElementById('questionsContainer')
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+  ];
+  for(var i=0; i< questions.length; i++){
+
+  }
 
 function startQuiz(){
     //console.log("You clicked the button!")
     var startContainer = document.getElementById('startContainer');
     startContainer.setAttribute("class", "start hide");
-};
+
+    questionsEl.setAttribute("class", "")
+
+    nextQuestion();
+}
+
+function nextQuestion(){
+   var currentQuestion = questions[nextQuestionIndex];
+   questionsEl.children[0].textContent = currentQuestion.title;
+}
 
