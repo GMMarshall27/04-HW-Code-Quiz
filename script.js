8b26687b3abb0b21dbabf0f3395b037a40559d9b
GMMarshall <gmmarshall27@gmail.com>
2021-09-22 16:32:03 -0400

    got question choices to appear on screen as buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var nextQuestionIndex=0;
 var startButton = document.getElementById('startBtn');
 startButton.addEventListener('click', startQuiz);
-var answersEl = document.getElementById('answers')
+var choicesEl = document.getElementById('choices')
 var questionsEl = document.getElementById('questionsContainer')
 var questions = [
     {
@@ -9,10 +9,25 @@ var questions = [
       choices: ["strings", "booleans", "alerts", "numbers"],
       answer: "alerts"
     },
-  ];
-  for(var i=0; i< questions.length; i++){
+    {
+        title: "Inside which HTML element do we put the JavaScript",
+        choices: ["<script>", "<scripting>", "<js>","<javascript>"],
+        answer: "<script>"
+    },
 
-  }
+    {
+        title: "How do you write 'Hello World' in an alert box?",
+        choices: ["msgBox('Hello Word')", "alertBox('Hello World')", "msg('Hello World')", "alert('Hello World')"],
+        answer: "alert('Hello World')"
+    },
+
+    {
+        title: "Which of these is Not a flex direction property?",
+        choices: ["row", "column", "wrap", "column-reverse"],
+        answer: "wrap"
+    }
+
+  ];
 
 function startQuiz(){
     //console.log("You clicked the button!")
@@ -27,5 +42,13 @@ function startQuiz(){
 function nextQuestion(){
    var currentQuestion = questions[nextQuestionIndex];
    questionsEl.children[0].textContent = currentQuestion.title;
+
+
+for(var i = 0; i < currentQuestion.choices.length; i++){
+    var choiceBtn = document.createElement("button");
+    choiceBtn.textContent = currentQuestion.choices[i];
+
+    choicesEl.appendChild(choiceBtn);
 }
 
+}
